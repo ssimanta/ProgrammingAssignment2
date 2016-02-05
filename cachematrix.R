ecada51b0631d8093c5cd4543a1467a183fcc34d
ssimanta <simanta@hotmail.com>
2016-02-05 14:06:03 -0600

    Modified cachematrix.R for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+##
+##
+##  SS on 02/05/2016
+##  Function Name: makeCacheMatrix 
+##  Description :
+##  Description :This function accepts a inversible Square Matrix as an input Parameter, 
+##               and returns a list of following  functions:
+##               set     = Store the input matrix into an environment variable 
+##               get     = Returns the matrix previously stored in the environment variable(x)
+##               setinv  = Store the inverse of the matrix in the environment variable
+##               getinv  = Returns the inverse previously stored in the environment variable(inv)
+##
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix())
+{
+  inv <- NULL 
+  set <- function(y)
+  {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set=set, get=get,setinv=setinv,getinv=getinv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##
+##  Function Name: cacheSolve 
+##  Description  :  This function accepts the output of the function makeCacheMatrix 
+##                  and returns the inverse of the Matrix.
+##
+##
+cacheSolve <- function(x,...)
+{
+  inv = x$getinv()
+  # if inverse exists then just retrun the value.
+  if( !is.null(inv))
+  {
+      message("Retrieving catched data")
+      return(inv)
+  }
+  # If inverse does not exist, calculate and return it.
+  mat.data <- x$get()
+  inv   <- solve(mat.data)
+  x$setinv(inv)
+  inv
 }
